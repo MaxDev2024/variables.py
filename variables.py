41825f3fc809fdda1fef1e92246adbcf6919f83b
MaxDev2024 <maksimkalacky2009@gmail.com>
2024-10-24 15:05:15 +0900

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 completed_dz = 12
 hours_spent = 1.5
 course_name = 'Python'
-time_for_one_task = int(completed_dz / hours_spent)
+time_for_one_task = hours_spent / completed_dz
 print('Kypc:',course_name,',всего задач:',completed_dz,',затрачено часов:',hours_spent,',среднее время выполнения:',time_for_one_task,'часов')
